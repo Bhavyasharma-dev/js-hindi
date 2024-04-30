0c900d084e0ba75b4d3ed6f33f9289d62077e90e
bhavya1134 <168431720+bhavya1134@users.noreply.github.com>
2024-04-30 09:18:20 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict "//treat all js code as newer version
+
+console.log(3+3);
+
+let name ="hitesh"
+let age = 18
+let isloggedin =false 
+
+//bigint
+//string =""
+//boolean=true/false
+//null=standalone value
+//undefined = let state;
+//symbol = unique 
+
+//object 
+console.log(typeof "hitesh")// return string 
+//undefined type= undefined ,,,,, nulle type= object 
