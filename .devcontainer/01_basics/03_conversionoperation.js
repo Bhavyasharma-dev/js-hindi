d9aa46cebaa02dcdc85cdaacc3ab4e82f6991411
bhavya1134 <168431720+bhavya1134@users.noreply.github.com>
2024-04-30 10:49:10 +0000

    conersion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+ldt score ="33"
+
+console.log(typeof score );
+console.log(typeof(score));//method
+
+let valueinnumber= Number(score)
+console.log(typeof valueinnumber );
+console.log(valueinnumber);
+
+//"33"= 33
+//"33abc"= nan
+
+let isloggedin = 1
+let booleanisloggedin = boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// 1 =true , 0=false 
+// ""= false 
+//"hitesh "= true
