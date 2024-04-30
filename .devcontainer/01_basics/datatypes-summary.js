e12eab52922755204f4b184e905cd00514d6175f
bhavya1134 <168431720+bhavya1134@users.noreply.github.com>
2024-04-30 17:14:17 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+//primitive 
+//types= string,number,boolean,null ,undefined,symbol,bigint.
+const score=100
+const scorevalue=100.3
+
+const isloggedin=false
+const outsidetemp=null
+let username;
+
+const id =symbol('123')
+const anotherid= symbol('123')
+//both id and anotherid will have different datatype
+
+
+//reference (non primitive)
+
+// array,object,functions
+
+const heros =[a,b,c]
+
+let myobj ={
+    name:"hitesh"
+    age: 22,
+}
+
+
+const myfunction= function(){
+    console.log("hello world");
+}
+
