38e6c4d2bc996e7d8817b0f8d59964d6ab9b7766
bhavya1134 <168431720+bhavya1134@users.noreply.github.com>
2024-04-30 11:54:43 +0000

    conversion of datatype

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -17,3 +17,31 @@ console.log(booleanisloggedin);
 // 1 =true , 0=false 
 // ""= false 
 //"hitesh "= true
+
+
+
+let value = 3 
+let negvalue = -value 
+console.log(negvalue);
+
+
+let string1= "hello"
+let string2= "hitesh"
+
+let string3 = string1 + string2
+ console.log(string3);
+
+
+ console.log("1"+ 2 );
+ console.log(1+ "2");
+
+ console.log("1" + 2 +2 );
+ console.log(1+2+"2");
+
+ let num1,num2,num3
+ num1=num2=num3= 2+2
+
+ let gamecounter =100
+ ++gamecounter ;
+ console.log(gamecounter);
+ 
